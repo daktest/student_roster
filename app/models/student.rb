210d962c3fefadebfb4390a0d4fcbb18b5dc6cdf
Dave Kubasiak <david.kubasiak@itron.com>
2017-11-13 15:37:11 -0500

    Finished student roster homework #1
     - added validations to model
     - added `email_validator` gem to validate email addresses
    
    [#152260936]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,10 @@
 class Student < ApplicationRecord
+  validates :first_name, :last_name, :email, :cell_phone, :home_phone,
+            :work_phone, presence: true
+  validates :email, email: true
+  validates :cell_phone, :home_phone, :work_phone,
+            format: {
+              with: /\d\d\d-\d\d\d-\d\d\d\d/,
+              message: 'should be like xxx-xxx-xxxx'
+            }
 end
