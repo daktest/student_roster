d112d0175c6a141b2e93a173b51d77890085fb15
Dave Kubasiak <david.kubasiak@itron.com>
2017-11-14 09:37:06 -0500

    Modified validations to email
     * added a case-insensitive `uniqueness` validation to the email field
    
    [#152260936]

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,7 @@ class Student < ApplicationRecord
   validates :first_name, :last_name, :email, :cell_phone, :home_phone,
             :work_phone, presence: true
   validates :email, email: true
+  validates :email, uniqueness: { case_sensitive: false }
   validates :cell_phone, :home_phone, :work_phone,
             format: {
               with: /\d\d\d-\d\d\d-\d\d\d\d/,
